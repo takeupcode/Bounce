d0504b6f96afc74be60686d9506e699868253aae
Wahid Tanner <wahidtanner@gmail.com>
2016-07-23 00:17:03 -0400

    Added ability to control left and right movement.

--- Bounce/BounceGame.cpp
+++ Bounce/BounceGame.cpp
@@ -36,6 +36,24 @@ BounceGame::~BounceGame ()
 void BounceGame::handleInput ()
 {
     getWindow()->handleInput();
+    
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+    {
+        mPositionDelta.x -= 10.0f;
+        if (mPositionDelta.x < -500.0f)
+        {
+            mPositionDelta.x = -500.0f;
+        }
+    }
+    
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+    {
+        mPositionDelta.x += 10.0f;
+        if (mPositionDelta.x > 500.0f)
+        {
+            mPositionDelta.x = 500.0f;
+        }
+    }
 }
 
 void BounceGame::update ()
@@ -53,6 +71,14 @@ void BounceGame::render ()
 void BounceGame::moveDot ()
 {
     float elapsedSeconds = elapsed().asSeconds();
+    
+    if ((mDot.getPosition().x + mSphereTextureSize.x / 2 > getWindow()->size().x &&
+         mPositionDelta.x > 0) ||
+        (mDot.getPosition().x - mSphereTextureSize.x / 2 < 0 &&
+         mPositionDelta.x < 0))
+    {
+        mPositionDelta.x = -mPositionDelta.x;
+    }
     
     if ((mDot.getPosition().y + mSphereTextureSize.y / 2 > getWindow()->size().y &&
          mPositionDelta.y > 0) ||
